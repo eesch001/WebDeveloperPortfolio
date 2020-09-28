84e50b01dd5a3f66ab8a91fcb7cf998c65b7fd2e
eesch001 <eesch001@ucr.edu>
2020-09-28 16:50:39 -0700

    Updates

---
+++ precache-manifest.d67d359c3894ac3c48bc27058abf96ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c3fda2954ffc2c69d8963dc08ec978",
+    "url": "/WebDeveloperPortfolio/index.html"
+  },
+  {
+    "revision": "037d1ef3a8c9d859c9a2",
+    "url": "/WebDeveloperPortfolio/static/css/main.86d374cc.chunk.css"
+  },
+  {
+    "revision": "994586592181da2c2cec",
+    "url": "/WebDeveloperPortfolio/static/js/2.33fe83dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebDeveloperPortfolio/static/js/2.33fe83dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037d1ef3a8c9d859c9a2",
+    "url": "/WebDeveloperPortfolio/static/js/main.2d5fc0fb.chunk.js"
+  },
+  {
+    "revision": "e4e909e17b3d3c686a1d",
+    "url": "/WebDeveloperPortfolio/static/js/runtime-main.ef69b297.js"
+  },
+  {
+    "revision": "f9a93d85429f0d97667f9edfd453b836",
+    "url": "/WebDeveloperPortfolio/static/media/craigslist.f9a93d85.jpg"
+  },
+  {
+    "revision": "051524a8fb77cacd3346c3f2f367c0f0",
+    "url": "/WebDeveloperPortfolio/static/media/doggierope.051524a8.jpg"
+  },
+  {
+    "revision": "253064428a05b2b5e805eaa2d44dc001",
+    "url": "/WebDeveloperPortfolio/static/media/horrorhospital.25306442.jpg"
+  },
+  {
+    "revision": "60783922462b27f786301f116ebd0f12",
+    "url": "/WebDeveloperPortfolio/static/media/portfolioProfilePic.60783922.jpg"
+  },
+  {
+    "revision": "3cfa9ac7301c4903aaa83b8685dd5fa1",
+    "url": "/WebDeveloperPortfolio/static/media/quizbook.3cfa9ac7.jpg"
+  },
+  {
+    "revision": "bd614d78525c20d4e44763a43f93692c",
+    "url": "/WebDeveloperPortfolio/static/media/tree.bd614d78.jpg"
+  }
+]);
